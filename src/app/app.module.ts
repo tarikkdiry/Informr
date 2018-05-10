9987f285f3937a8fe44a4005cbe49400356f8f90
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-05-10 00:45:02 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ArticleComponent } from './article/article.component';
+import { InfoComponent } from './info/info.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ArticleComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
