d824b64301d90411e70fc272c3c1505d9a65e91f
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-05-13 21:44:43 -0400

    reversed coupling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
-import { InfoComponent } from './info/info.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     ArticleComponent,
-    InfoComponent,
     NavbarComponent
   ],
   imports: [
